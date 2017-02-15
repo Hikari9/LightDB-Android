825cf2e67d8dee77c76f0b10ae8de653c636536b
Rico Tiongson <thericotiongson@gmail.com>
2017-02-15 11:22:35 +0800

    Added doc comments to ForeignKey class.

--- lightdb/src/main/java/io/hikari9/lightdb/ForeignKey.java
+++ lightdb/src/main/java/io/hikari9/lightdb/ForeignKey.java
@@ -1,24 +1,78 @@
 package io.hikari9.lightdb;
 
 import android.database.Cursor;
+import android.support.annotation.Nullable;
 
+/**
+ * A class to wrap a foreign key pointer to another database model.
+ * @param <T> wrap a primary key pointer to another LightModel
+ */
 public class ForeignKey<T extends LightModel> {
+
+    // reference to the model class used for model retrieval queries
     Class<T> model;
+
+    // id of entity pointed by this foreign key
     Long id;
-    ForeignKey(Class<T> model, Long id) {
+
+    /**
+     * Construct a foreign key reference based on a LightModel and a given id (can be null).
+     * @param model the class instance of the database model referred by this foreign key
+     * @param id the id of the entity pointed by this foreign key
+     */
+    public ForeignKey(Class<T> model, @Nullable Long id) {
         this.model = model;
         this.id = id;
     }
-    void setId(Long id) {
+
+    /**
+     * Sets the id of this foreign key.
+     * @param id the id of the entity pointed by this foreign key
+     */
+    public void setId(@Nullable Long id) {
         this.id = id;
     }
-    Long getId() {
+
+    /**
+     * Gets the id of this foreign key.
+     * @return id the id of the entity pointed by this foreign key
+     */
+    public Long getId() {
         return id;
     }
-    T retrieve() {
+
+    /**
+     * Sets the id of this foreign key based on referred database model instance.
+     * @param instance a LightModel object that this foreign key will refer
+     */
+    public void setModel(T instance) {
+        if (instance == null) setId(null);
+        else setId(instance.getId());
+    }
+
+    /**
+     * Gets the model referred by this foreign key.
+     * @return the LightModel object referred by this foreign key, null if reference does not exist
+     */
+    public T getModel() {
         return LightDatabase.findById(model, id);
     }
-    Cursor retrieveCursor() {
+
+    /**
+     * Gets a Cursor to the query used to retrieve the entity referred by this foreign key.
+     * @return a Cursor object that this foreign key refers
+     */
+    public Cursor getModelCursor() {
         return LightDatabase.findCursorById(model, id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode() * 27 + model.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ForeignKey<" + model.getSimpleName() + ":" + getId() + ">";
     }
 }
